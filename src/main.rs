668eae9259d016321d3f9026f62e437d61d5e9fb
Louis Quinnell <_@ziorobo.co>
2022-08-16 08:39:15 +1000

    Add a simple synthesis to file example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use aws_sdk_polly::model::{OutputFormat, VoiceId};
+use aws_sdk_polly::{Client, Error};
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    synthesize(&client, "this is some text").await
+}
+
+async fn synthesize(client: &Client, text: &str) -> Result<(), Error> {
+    let resp = client
+        .synthesize_speech()
+        .output_format(OutputFormat::Mp3)
+        .text(text)
+        .voice_id(VoiceId::Joanna)
+        .send()
+        .await?;
+
+    // Get MP3 data from response and save it
+    let mut blob = resp
+        .audio_stream
+        .collect()
+        .await
+        .expect("failed to read data");
+
+    let parts: Vec<&str> = text.split('.').collect();
+    let out_file = format!("{}{}", String::from(parts[0]), ".mp3");
+
+    let mut file = tokio::fs::File::create(out_file)
+        .await
+        .expect("failed to create file");
+
+    file.write_all_buf(&mut blob)
+        .await
+        .expect("failed to write to file");
+
+    Ok(())
 }
