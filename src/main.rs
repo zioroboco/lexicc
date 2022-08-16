438029e5691af1da16e64fe28c4db21c0562ead0
Louis Quinnell <_@ziorobo.co>
2022-08-16 10:59:33 +1000

    Play resulting audio with rodio

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use aws_sdk_polly::model::{OutputFormat, VoiceId};
-use aws_sdk_polly::{Client, Error};
+use aws_sdk_polly::model::{Engine, OutputFormat, VoiceId};
+use aws_sdk_polly::Client;
+use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+use std::io::BufReader;
 use tokio::io::AsyncWriteExt;
 
+const FILENAME: &str = "out.ogg";
+const TEXT: &str = "Hello, world!";
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let shared_config = aws_config::load_from_env().await;
     let client = Client::new(&shared_config);
-    synthesize(&client, "this is some text").await
-}
 
-async fn synthesize(client: &Client, text: &str) -> Result<(), Error> {
     let resp = client
         .synthesize_speech()
-        .output_format(OutputFormat::Mp3)
-        .text(text)
+        .output_format(OutputFormat::OggVorbis)
+        .text(TEXT)
         .voice_id(VoiceId::Joanna)
+        .engine(Engine::Neural)
         .send()
         .await?;
 
-    // Get MP3 data from response and save it
     let mut blob = resp
         .audio_stream
         .collect()
         .await
         .expect("failed to read data");
 
-    let parts: Vec<&str> = text.split('.').collect();
-    let out_file = format!("{}{}", String::from(parts[0]), ".mp3");
-
-    let mut file = tokio::fs::File::create(out_file)
+    let mut file = tokio::fs::File::create(FILENAME)
         .await
         .expect("failed to create file");
 
     file.write_all_buf(&mut blob)
         .await
         .expect("failed to write to file");
+
+    let (_stream, stream_handle) = OutputStream::try_default().expect("failed to create stream");
+    let sink = Sink::try_new(&stream_handle).expect("failed to create sink");
+
+    let file = BufReader::new(File::open(FILENAME).expect("failed to open file"));
+    let source = Decoder::new_vorbis(file).expect("failed to create decoder");
+
+    sink.append(source);
+
+    sink.sleep_until_end();
 
     Ok(())
 }
