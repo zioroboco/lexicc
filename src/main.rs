0768827da26e950df2e857421ed688bf1fe2f5a1
Louis Quinnell <_@ziorobo.co>
2022-08-18 19:02:34 +1000

    Break input text into paragraph chunks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use aws_sdk_polly::model::{Engine, OutputFormat, TextType, VoiceId};
 use aws_sdk_polly::Client;
 use rodio::{Decoder, OutputStream, Sink};
+use std::collections::hash_map::DefaultHasher;
 use std::fs::DirEntry;
+use std::hash::{Hash, Hasher};
 use std::io::BufReader;
 use std::path::PathBuf;
 use std::time::Duration;
@@ -10,9 +12,9 @@ use tokio::io::AsyncWriteExt;
 
 const APPNAME: &str = "lexicc";
 
-fn process_text(text: &str) -> String {
+fn process_text(text: String) -> String {
     format!(
-        r#"<speak><prosody rate="x-fast">{}<break/></prosody></speak>"#,
+        r#"<speak><prosody rate="x-fast"><p>{}</p></prosody></speak>"#,
         text.replace('"', "&quot;")
             .replace('&', "&amp;")
             .replace('\'', "&apos;")
@@ -35,6 +37,12 @@ fn entries_from(path: &PathBuf) -> Vec<DirEntry> {
         .collect();
     entries.sort_by_key(|entry| entry.path());
     entries
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
 
 #[tokio::main]
@@ -61,6 +69,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
+    let mut paragraphs: Vec<String> = Vec::new();
+
     loop {
         let inbox_entries = entries_from(&inbox_dir);
         if inbox_entries.is_empty() && sink.empty() {
@@ -68,13 +78,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         for entry in inbox_entries {
-            let text = std::fs::read_to_string(&entry.path())?;
-            let processed_text = process_text(&text);
+            for chunk in std::fs::read_to_string(&entry.path())?.split('\n') {
+                if chunk.is_empty() {
+                    continue;
+                }
+                paragraphs.push(chunk.to_string());
+            }
+            std::fs::remove_file(&entry.path())?;
+        }
+
+        while sink.len() < 2 && !paragraphs.is_empty() {
+            let processed_text = process_text(paragraphs.pop().unwrap());
+            let hash = calculate_hash(&processed_text).to_string();
 
             let synth_output = client
                 .synthesize_speech()
                 .output_format(OutputFormat::OggVorbis)
-                .voice_id(VoiceId::Joanna)
+                .voice_id(VoiceId::Olivia)
                 .engine(Engine::Neural)
                 .text_type(TextType::Ssml)
                 .text(processed_text)
@@ -82,14 +102,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .await?;
 
             let mut blob = synth_output.audio_stream.collect().await?;
-            let mut file = tokio::fs::File::create(output_dir.join(entry.file_name())).await?;
+            let mut file = tokio::fs::File::create(output_dir.join(&hash)).await?;
             file.write_all_buf(&mut blob).await?;
 
-            let file = BufReader::new(std::fs::File::open(output_dir.join(entry.file_name()))?);
+            let file = BufReader::new(std::fs::File::open(output_dir.join(&hash))?);
             let source = Decoder::new_vorbis(file)?;
             sink.append(source);
-
-            std::fs::remove_file(&entry.path())?;
         }
 
         std::thread::sleep(Duration::from_millis(1000));
